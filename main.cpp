ba13c4f278fd12cbf93bdeeb2457e11af2fb9f1a
Zach Burkhardt <zburkhardt18@gmail.com>
2018-09-21 18:02:07 -0400

    Finished adding comments and answering questions

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@
 #include <iostream> // for input-output stream operations
 #include <iomanip> // for providing parametric manipulators operations
 #include <limits> // for providing elements with the characteristics of arithmetic types
-using std::cout;
-using std::cin;
-using std::endl;
+using std::cout; // qualifies cout element in standard namespace; for displaying console output
+using std::cin; // qualifies cin element in standard namespace; for accepting input
+using std::endl; // qualifies endl element in standard namespace; for ending line
 
 int main()
 {
@@ -33,11 +33,11 @@ int main()
         if ((cin.fail()) || organisms < 2) // decision construct tests 2 conditions
         {
             cout << "The starting number of organisms " // output stream a message
-                 << "must be a valid number that is at least 2.\n";
+                 << "must be a valid number that is at least 2.\n"; // continues message on same line
             cin.clear(); // clear error flag for cin
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignores input in buffer until the line break
         }
-        else
+        else // runs if above "if" statement is false
         {
             loopFlag = false; // change Boolean value to end this loop (good input!)
         }
@@ -49,14 +49,19 @@ int main()
 /*
 
 What does the using std::..., do for us, and why would we use this instead of using std namespace?
-
+    Using std::... explicitly qualifies the standard namespace for a specific element. We would want to do
+    this to avoid name collisions, which occur when an element is qualified by more than one namespace.
 
 What about cin.fail() for console input?
     Cin.fail() checks if the inputed value is compatible with the variable type.
 
 What was a short-circuit evaluation?
-
+    If the leftmost subexpression in a logical expression using && or || gives enough information to
+    determine the final value of the expression, then C++ does not evaluate the second expression.
 
 What about the parameters for ignore, the numeric limits and streamsize?
+    Cin.ignore will ignore input in the buffer until the next line break. Numeric limit tells cin.ignore to
+    ignore up to the maximum value of the specified type, which in this case is defined by streamsize as
+    the size of the buffer.
 
 */
